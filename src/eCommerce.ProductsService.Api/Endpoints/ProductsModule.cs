ec8f26cef5e3187750482c2d0ded033b1c5ac849
Adrian Mechato <cesar.mechatov@externos.bci.cl>
2025-11-29 13:41:34 -0500

    [ADD] Dockerfile

--- src/eCommerce.ProductsService.Api/Endpoints/ProductsModule.cs
+++ src/eCommerce.ProductsService.Api/Endpoints/ProductsModule.cs
@@ -10,89 +10,89 @@ using eCommerce.ProductsService.Application.UseCases.Products.Queries.GetProduct
 
 namespace eCommerce.ProductsService.Api.Endpoints;
 
-public class ProductsModule : ICarterModule
-{
-    public void AddRoutes(IEndpointRouteBuilder app)
-    {
-        var group = app.MapGroup("api/product")
-            .WithTags("Products");
+//public class ProductsModule : ICarterModule
+//{
+//    public void AddRoutes(IEndpointRouteBuilder app)
+//    {
+//        var group = app.MapGroup("api/product")
+//            .WithTags("Products");
 
-        group.MapGet("", GetAllProducts)
-            .WithName("GetAllProducts")
-            .Produces<BaseResponse<IEnumerable<GetAllProductsResponseDto>>>();
+//        group.MapGet("", GetAllProducts)
+//            .WithName("GetAllProducts")
+//            .Produces<BaseResponse<IEnumerable<GetAllProductsResponseDto>>>();
 
-        group.MapGet("{productID:guid}", GetProductById)
-            .WithName("GetProductById")
-            .Produces<BaseResponse<GetAllProductsResponseDto>>();
+//        group.MapGet("{productID:guid}", GetProductById)
+//            .WithName("GetProductById")
+//            .Produces<BaseResponse<GetAllProductsResponseDto>>();
 
-        group.MapPost("", CreateProduct)
-            .WithName("CreateProduct")
-            .Produces<BaseResponse<bool>>();
+//        group.MapPost("", CreateProduct)
+//            .WithName("CreateProduct")
+//            .Produces<BaseResponse<bool>>();
 
-        group.MapPut("", UpdateProduct)
-            .WithName("UpdateProduct")
-            .Produces<BaseResponse<bool>>();
+//        group.MapPut("", UpdateProduct)
+//            .WithName("UpdateProduct")
+//            .Produces<BaseResponse<bool>>();
 
-        group.MapDelete("{productID:guid}", DeleteProduct)
-            .WithName("DeleteProduct")
-            .Produces<BaseResponse<bool>>();
-    }
+//        group.MapDelete("{productID:guid}", DeleteProduct)
+//            .WithName("DeleteProduct")
+//            .Produces<BaseResponse<bool>>();
+//    }
 
-    private static async Task<IResult> GetAllProducts(
-        [AsParameters] GetAllProductsQuery query,
-        IDispatcher dispatcher,
-        CancellationToken cancellationToken)
-    {
-        var response = await dispatcher.Dispatch<GetAllProductsQuery,
-            IEnumerable<GetAllProductsResponseDto>>(query, cancellationToken);
+//    private static async Task<IResult> GetAllProducts(
+//        [AsParameters] GetAllProductsQuery query,
+//        IDispatcher dispatcher,
+//        CancellationToken cancellationToken)
+//    {
+//        var response = await dispatcher.Dispatch<GetAllProductsQuery,
+//            IEnumerable<GetAllProductsResponseDto>>(query, cancellationToken);
 
-        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
-    }
+//        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
+//    }
 
-    private static async Task<IResult> GetProductById(
-        Guid productID,
-        IDispatcher dispatcher,
-        CancellationToken cancellationToken)
-    {
-        var query = new GetProductByIdQuery { ProductID = productID };
-        var response = await dispatcher.Dispatch<GetProductByIdQuery,
-            GetAllProductsResponseDto>(query, cancellationToken);
+//    private static async Task<IResult> GetProductById(
+//        Guid productID,
+//        IDispatcher dispatcher,
+//        CancellationToken cancellationToken)
+//    {
+//        var query = new GetProductByIdQuery { ProductID = productID };
+//        var response = await dispatcher.Dispatch<GetProductByIdQuery,
+//            GetAllProductsResponseDto>(query, cancellationToken);
 
-        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
-    }
+//        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
+//    }
 
-    private static async Task<IResult> CreateProduct(
-        CreateProductCommand command,
-        IDispatcher dispatcher,
-        CancellationToken cancellationToken)
-    {
-        var response = await dispatcher
-            .Dispatch<CreateProductCommand, bool>(command, cancellationToken);
+//    private static async Task<IResult> CreateProduct(
+//        CreateProductCommand command,
+//        IDispatcher dispatcher,
+//        CancellationToken cancellationToken)
+//    {
+//        var response = await dispatcher
+//            .Dispatch<CreateProductCommand, bool>(command, cancellationToken);
 
-        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
-    }
+//        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
+//    }
 
-    private static async Task<IResult> UpdateProduct(
-        UpdateProductCommand command,
-        IDispatcher dispatcher,
-        CancellationToken cancellationToken)
-    {
-        var response = await dispatcher
-            .Dispatch<UpdateProductCommand, bool>(command, cancellationToken);
+//    private static async Task<IResult> UpdateProduct(
+//        UpdateProductCommand command,
+//        IDispatcher dispatcher,
+//        CancellationToken cancellationToken)
+//    {
+//        var response = await dispatcher
+//            .Dispatch<UpdateProductCommand, bool>(command, cancellationToken);
 
-        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
-    }
+//        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
+//    }
 
-    private static async Task<IResult> DeleteProduct(
-        Guid productID,
-        IDispatcher dispatcher,
-        CancellationToken cancellationToken)
-    {
-        var response = await dispatcher
-            .Dispatch<DeleteProductCommand, bool>(
-                new DeleteProductCommand { ProductID = productID },
-                cancellationToken);
+//    private static async Task<IResult> DeleteProduct(
+//        Guid productID,
+//        IDispatcher dispatcher,
+//        CancellationToken cancellationToken)
+//    {
+//        var response = await dispatcher
+//            .Dispatch<DeleteProductCommand, bool>(
+//                new DeleteProductCommand { ProductID = productID },
+//                cancellationToken);
 
-        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
-    }
-}
+//        return response.IsSuccess ? Results.Ok(response) : Results.BadRequest(response);
+//    }
+//}
